747657bfcc27af368733506eb9f295342d1bf50b
Joe Lipper <lipperjj@gmail.com>
2014-06-20 18:43:38 -0700

    added album collection state and player bar template at the bottom of collection.html and album.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,6 +36,12 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
     controller: 'Collection.controller',
     templateUrl: '/templates/collection.html'
   });
+
+  $stateProvider.state('album', {
+    url: '/album',
+    templateUrl: '/templates/album.html',
+    controller: 'Album.controller'
+  });
 }]);
 
 blocJams.controller('Landing.controller', ['$scope', function($scope) {
@@ -65,3 +71,47 @@ blocJams.controller('Collection.controller', ['$scope', function($scope) {
     $scope.albums.push(angular.copy(albumPicasso));
   }
 }]);
+
+
+blocJams.controller('Album.controller', ['$scope', function($scope) {
+  $scope.album = angular.copy(albumPicasso);
+
+  var hoveredSong = null;
+  var playingSong = null;
+
+  $scope.onHoverSong = function(song) {
+    hoveredSong = song;
+  };
+
+  $scope.offHoverSong = function(song) {
+    hoveredSong = null;
+  };
+
+  $scope.getSongState = function(song) {
+    if (song === playingSong) {
+      return 'playing';
+    }
+    else if (song === hoveredSong) {
+      return 'hovered';
+    }
+    return 'default';
+  };
+
+  $scope.playSong = function(song) {
+    playingSong = song;
+  };
+
+  $scope.pauseSong = function(song) {
+    playingSong = null;
+  };
+  
+}]);
+
+
+
+
+
+
+
+
+
