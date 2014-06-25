2daba63a1dff27e9b14aed8ba2f5e53094625908
Joe Lipper <lipperjj@gmail.com>
2014-06-25 11:14:24 -0700

    added timecode filter and functional time update

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -383,9 +383,17 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
 
 blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
+
+  SongPlayer.onTimeUpdate(function(event, time){
+    $scope.$apply(function(){
+      $scope.playTime = time;
+    });
+  });
+
 }]);
 
-blocJams.service('SongPlayer', function() {
+
+blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
   var currentSoundFile = null;
 
   var trackIndex = function(album, song) {
@@ -432,6 +440,9 @@ blocJams.service('SongPlayer', function() {
         currentSoundFile.setTime(time);
       }
     },
+    onTimeUpdate: function(callback) {
+      return $rootScope.$on('sound:timeupdate', callback);
+    },
     setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
@@ -442,11 +453,15 @@ blocJams.service('SongPlayer', function() {
         formats: [ "mp3" ],
         preload: true
       });
+  
+      currentSoundFile.bind('timeupdate', function(e){
+        $rootScope.$broadcast('sound:timeupdate', this.getTime());
+      });  
    
       this.play();
     }
   };
-});
+}]);
 
 blocJams.directive('slider', ['$document', function($document){
   // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
@@ -540,7 +555,34 @@ blocJams.directive('slider', ['$document', function($document){
   };
 }]);
 
+blocJams.filter('timecode', function(){
+  return function(seconds) {
+    seconds = Number.parseFloat(seconds);
 
+    // Returned when no time is provided.
+    if (Number.isNaN(seconds)) {
+      return '-:--';
+    }
+
+    // make it a whole number
+    var wholeSeconds = Math.floor(seconds);
+
+    var minutes = Math.floor(wholeSeconds / 60);
+
+    remainingSeconds = wholeSeconds % 60;
+
+    var output = minutes + ':';
+
+    // zero pad seconds, so 9 seconds should be :09
+    if (remainingSeconds < 10) {
+      output += '0';
+    }
+
+    output += remainingSeconds;
+
+    return output;
+  }
+})
 
 
 
